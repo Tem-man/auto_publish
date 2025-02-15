ab1dda6b21b6487faebc78d6b51232e498f826a3
Temman_lin <Temman_lin@qq.com>
2025-02-15 15:51:47 +0800

    feat: mofiy

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,37 @@
 <script setup>
 import { ref } from 'vue'
-
-defineProps({
-  msg: String,
-})
-
 const count = ref(0)
+const handleCount = (a) => {
+  console.log("dsdll", a);
+  if (a === 1) {
+    count.value += 1;
+  } else {
+    count.value -= 1;
+  }
+}
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+    <button type="button" @click="handleCount(1)">+1</button>
+    <button type="button" @click="handleCount(2)">-1</button>
+    <div class="count">{{ count }}</div>
   </div>
 
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+
 </template>
 
 <style scoped>
+.count {
+  margin-top: 10px;
+  padding: 5px 0;
+  border-radius: 6px;
+  background-color: aliceblue;
+  font-size: 16px;
+  font-weight: 500;
+}
+
 .read-the-docs {
   color: #888;
 }
